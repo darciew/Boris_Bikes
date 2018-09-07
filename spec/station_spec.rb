a16d003799bdddb6480316c8506a7715d7cb528a
Antonio <antoniodaria@msn.com>
2018-09-07 16:57:44 +0100

    Add error if no bikes available

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,12 +3,27 @@ require 'station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-    it "releases a bike when we ask it to" do
-     expect(DockingStation.new.release_bike).to be_a_kind_of(Bike)
+    # it "releases a bike when we ask it to" do
+    #  expect(DockingStation.new.release_bike).to be_a_kind_of(Bike)
+    # end
+
+    describe '#release_bike' do
+      it 'raises an error' do
+        expect { subject.release_bike }.to raise_error "No bikes available"
+      end
+
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
     end
 
-    it "releases a working bike" do
-      expect(DockingStation.new.release_bike.working?).to eq(true)
+
+    it "expect the working method of a released bike to be true" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike.working?).to eq true
     end
 
     # we want to be able to dock Bike at DockingStation.new
@@ -23,4 +38,5 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.bike).to eq bike
     end
+
 end
